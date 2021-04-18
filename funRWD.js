c271af1138aa19a4b4913d2ce1bccd2327a385a5
JarekJa <80716315+JarekJa@users.noreply.github.com>
2021-04-18 23:50:53 +0200

    Update funRWD.js

--- funRWD.js
+++ funRWD.js
@@ -143,3 +143,114 @@ function graj()
 	 l=0; 
     pom=0;
 }
+function opmenu() {
+  var x = document.getElementById("limenu");
+  var z = document.getElementById("prmemo");
+  if (x.style.display === "block") {
+	  z.style.display = "block";
+    x.style.display = "none";
+  } else {
+	  z.style.display = "none";
+    x.style.display = "block";
+  }
+}  
+
+		}
+	}
+}
+function ruchkom()
+{
+	var konty=true;
+	var tr=true;
+	var a,b,c,d;
+	var ab=12;
+	var tabpom=[1,2,3,4,5,6,7,8,9,10,11,12];
+	while(konty)
+	{
+	        tr=true;
+	while(tr)
+	{		
+		a=Math.floor(Math.random() * ab);
+		if(!Czyzbi(obrazki[tabpom[a]-1]))
+		{
+			tr=false;
+			c=tabpom[a]-1;
+			document.getElementById("t"+tabpom[a]).innerHTML="<img class=\"imgra\" src=\"r"+obrazki[c]+".jpg\" alt=\"Błąd\">";
+		}
+		tabpom.splice(a,1);
+		ab--;
+	}
+	tr=true;
+	while(tr)
+	{		
+		b=Math.floor(Math.random() * ab);
+		if(!Czyzbi(obrazki[tabpom[b]-1]))
+		{
+			tr=false;
+		    d=tabpom[b]-1;
+			document.getElementById("t"+tabpom[b]).innerHTML="<img class=\"imgra\" src=\"r"+obrazki[d]+".jpg\" alt=\"Błąd\">";
+		}
+				tabpom.splice(b,1);
+		ab--;
+	}
+	if(obrazki[c]==obrazki[d])
+	{
+	punkom++;
+	document.getElementById("pukogry").innerHTML="Punkty przeciwnika: "+punkom;
+	zbite.push(obrazki[c]);
+	}
+	else
+	{
+		konty=false;
+	}
+	}
+	setTimeout("czysc()", 3000);
+	setTimeout(function(){ document.getElementById("czyruch").innerHTML="Twój ruch"; 	l=0;	czyruch=true; }, 3000);
+}
+var l=0; 
+var pom;
+function ruch(n)
+{
+	if(czyruch)
+	{
+        if(!Czyzbi(obrazki[n-1]))
+		{
+			if(l==0)
+			{
+				document.getElementById("t"+n).innerHTML="<img src=\"r"+obrazki[n-1]+".jpg\" alt=\"Błąd\" width=\"150\" height=\"150\">";
+				pom=n;
+				l++;
+			}
+			else if(l==1 && n!=pom)
+			{
+				document.getElementById("t"+n).innerHTML="<img src=\"r"+obrazki[n-1]+".jpg\" alt=\"Błąd\" width=\"150\" height=\"150\">";
+				if(obrazki[pom-1]==obrazki[n-1])
+				{
+					pungra++;
+					document.getElementById("pugrgry").innerHTML="Twoje punkty: "+pungra;
+					zbite.push(obrazki[pom-1]);
+					l=0;
+				}
+				else
+				{
+										czyruch=false;
+					setTimeout("czysc()", 3000);
+					setTimeout(function(){ document.getElementById("czyruch").innerHTML="Ruch przeciwnika";ruchkom();  }, 3000);
+				}
+			}
+		}	
+	}
+}
+function graj()
+{
+	losujpol();
+	pungra=0;punkom=0;
+	document.getElementById("pugrgry").innerHTML="Twoje punkty: 0";
+	document.getElementById("pukogry").innerHTML="Punkty przeciwnika: 0";
+	document.getElementById("czyruch").innerHTML="Twój ruch";
+	czyruch=true;	
+	zbite= new Array(0);
+		czysc();
+	 l=0; 
+    pom=0;
+}
